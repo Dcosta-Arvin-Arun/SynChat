19a89c8af616223443560ea4db24d3e025142765
Dcosta Arvin Arun <arvin.23bce7971@vitapstudent.ac.in>
2026-02-21 20:16:38 +0530

    feat: set up authentication routes and controllers

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import authRoute from './routes/auth.route.js'
 
 const app = express();
+app.use('/api/auth', authRoute);
 
 app.listen(5001, () => {
-    console.log('Server is running on port 5001');
-});
+    console.log('server running on port 5001');
+})
