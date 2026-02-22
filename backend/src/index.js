0d0048451d3b3d85058a038273fdd44636bf2193
Dcosta Arvin Arun <arvin.23bce7971@vitapstudent.ac.in>
2026-02-22 20:05:52 +0530

    feat: implement database connection and user/message models

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,16 @@
 import express from 'express';
-import authRoute from './routes/auth.route.js'
+import authRoute from './routes/auth.route.js';
+import dotenv from 'dotenv';
+import { connectDB } from './lib/db.js';
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
+
 app.use('/api/auth', authRoute);
 
-app.listen(5001, () => {
-    console.log('server running on port 5001');
-})
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+    connectDB();
+});
