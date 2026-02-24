905d57979ff66dd596592adea87f07885989420a
Dcosta Arvin Arun <arvin.23bce7971@vitapstudent.ac.in>
2026-02-24 19:57:10 +0530

    feat: add protect routes middleware

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -40,7 +40,7 @@ export const signup = async (req, res) => {
         console.log("Error in signup controller", error.message);
         res.status(500).json({ message: "Internal server error" })
     }
-}
+};
 
 export const login = async (req, res) => {
     const { email, password } = req.body;
@@ -79,3 +79,7 @@ export const logout = (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const updateProfile = async (req, res) => {
+    
+}
