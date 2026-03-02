e1bf8e4747fc6c947be15e017a68b841ee9a32f8
Dcosta Arvin Arun <arvin.23bce7971@vitapstudent.ac.in>
2026-03-02 21:18:09 +0530

    fix: spa fallback fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,10 +29,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  const frontendPath = path.join(__dirname, "../frontend/dist");
 
-  app.get((req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.use(express.static(frontendPath));
+
+  // SPA fallback
+  app.use((req, res, next) => {
+    if (req.path.startsWith("/api")) return next();
+    res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
 
